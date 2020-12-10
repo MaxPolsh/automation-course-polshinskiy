9062d3f8eb360dae60ed6ed23d245999e0795b09
Max <maxmailpol@gmail.com>
2020-12-10 18:12:22 +0200

    lectures

---
+++ src/main/java/org/example/lectures/Arrays/ControlTask.java
@@ -0,0 +1,25 @@
+package org.example.lectures.Arrays;
+
+public class ControlTask {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print("# ");
+            }
+            System.out.println();
+
+        }
+        for (int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+                System.out.print("# ");
+
+
+            }
+
+
+            System.out.println();
+
+        }
+    }
+}
