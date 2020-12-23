1a2e9f97f4421e53434812921629841560351ad1
Max <maxmailpol@gmail.com>
2020-12-23 23:32:48 +0200

    homework7

--- src/main/java/org/example/infrastructure/ConfigurationManager.java
+++ src/main/java/org/example/infrastructure/ConfigurationManager.java
@@ -1,9 +1,22 @@
 package org.example.infrastructure;
 
 public class ConfigurationManager {
-    public static String getEnvironmentVariableOrDefault(String envVar, String defaulValue){
-        return System.getenv(envVar) != null && !System.getenv(envVar).isBlank() ?
-                System.getenv(envVar) : defaulValue;
+    private static ConfigurationManager singleton = new ConfigurationManager();
+    private ConfigurationManager(){
 
     }
+    public static ConfigurationManager getInstance(){
+        return singleton;
+    }
+
+    public static String getTestEnv(String envVar, String defaulValue){
+        return System.getenv(envVar) != null && !System.getenv(envVar).isBlank() ?
+                System.getenv(envVar) : defaulValue;
+    }
+
+    public static String getTestBrowser(String testBrowser, String defaulValue){
+        return System.getenv(testBrowser) != null && !System.getenv(testBrowser).isBlank() ?
+                System.getenv(testBrowser) : defaulValue;
+    }
 }
+
--- src/main/java/org/example/lectures/String/GetString.java
+++ src/main/java/org/example/lectures/String/GetString.java
@@ -4,6 +4,7 @@ import org.example.infrastructure.ConfigurationManager;
 
 public class GetString {
     public static void main(String[] args) {
-        System.out.println(ConfigurationManager.getEnvironmentVariableOrDefault("PATH", "1234"));
+        System.out.println(ConfigurationManager.getTestEnv("PATH", "1234"));
+        System.out.println(ConfigurationManager.getTestBrowser("Chrome", "EE"));
     }
 }
