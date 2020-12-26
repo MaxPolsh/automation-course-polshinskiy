f1b6ea1b9c170a7a426c798574545d2cd4403638
Max <maxmailpol@gmail.com>
2020-12-26 14:51:23 +0200

    homework7

--- src/main/java/org/example/infrastructure/ConfigurationManager.java
+++ src/main/java/org/example/infrastructure/ConfigurationManager.java
@@ -1,22 +1,27 @@
 package org.example.infrastructure;
 
 public class ConfigurationManager {
-    private static ConfigurationManager singleton = new ConfigurationManager();
+    private static ConfigurationManager instance;
     private ConfigurationManager(){
 
     }
     public static ConfigurationManager getInstance(){
-        return singleton;
+        if (instance == null)
+            instance = new ConfigurationManager();
+        return instance;
     }
 
-    public static String getTestEnv(String envVar, String defaulValue){
+    public String getTestEnv(){
+        return getEnvironmetVariableOrDefault("envVar", "defaulValue");
+    }
+
+    public String getTestBrowser(){
+        return getEnvironmetVariableOrDefault("envVar", "defaulValue");
+    }
+
+    private String getEnvironmetVariableOrDefault(String envVar, String defaulValue){
         return System.getenv(envVar) != null && !System.getenv(envVar).isBlank() ?
                 System.getenv(envVar) : defaulValue;
-    }
-
-    public static String getTestBrowser(String testBrowser, String defaulValue){
-        return System.getenv(testBrowser) != null && !System.getenv(testBrowser).isBlank() ?
-                System.getenv(testBrowser) : defaulValue;
     }
 }
 
--- src/main/java/org/example/lectures/String/GetString.java
+++ src/main/java/org/example/lectures/String/GetString.java
@@ -4,7 +4,7 @@ import org.example.infrastructure.ConfigurationManager;
 
 public class GetString {
     public static void main(String[] args) {
-        System.out.println(ConfigurationManager.getTestEnv("PATH", "1234"));
-        System.out.println(ConfigurationManager.getTestBrowser("Chrome", "EE"));
+        System.out.println(ConfigurationManager.getInstance().getTestEnv());
+        System.out.println(ConfigurationManager.getInstance().getTestBrowser());
     }
 }
