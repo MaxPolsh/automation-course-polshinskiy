c543ff78b410a37230c888c3949ab66e4ef28edb
Max <maxmailpol@gmail.com>
2021-03-10 20:48:29 +0200

    homework16

--- src/main/java/org/example/infrastructure/wdm/BrowserCapabilities.java
+++ src/main/java/org/example/infrastructure/wdm/BrowserCapabilities.java
@@ -1,16 +1,20 @@
 package org.example.infrastructure.wdm;
 
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserCapabilities {
     public static void main(String[] args) {
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+
         ChromeOptions options = new ChromeOptions();
 
-        options.setCapability("platformName", "Windows 10");
-        options.setCapability("browserName", "Chrome");
-        options.setCapability("browserVersion", "latest");
+        options.setExperimentalOption("w3c", true);
+        options.setCapability("platformName", "Windows 7");
+        options.setCapability("browserVersion", "latest-1");
+        options.setCapability("sauce:options", sauceOptions);
 
         WebDriver webDriver = new ChromeDriver(options);
 
